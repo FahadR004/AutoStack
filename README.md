178837b29e8791af1a00eedd5498c3d60bcc5286
FahadR004 <fahad.rasheed.004@gmail.com>
2025-12-30 02:08:10 +0500

    Installed inquirer. Worked on backend creation

---
+++ README.md
@@ -0,0 +1,10 @@
+### AutoMERN
+
+This npm package is designed to automate the tedious tasks of setting up MERN projects. 
+
+Simply automern -y for complete setup of backend 
+- MVC NodeJS architecture with MongoDB connection (enter your credentials in the env file)
+- React Vite Setup with TailwindCSS support
+
+Created by:
+Fahad Rasheed 
--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-function helloNpm() {
-  return "hello NPM"
+#!/usr/bin/env node
+
+import * as fs from 'node:fs/promises'
+import { open } from 'node:fs/promises'
+import inquirer from 'inquirer';
+import { select } from '@inquirer/prompts'
+
+
+const createFile = async (name, content, format = 'utf8') => {
+  try {
+      await fs.writeFile(name, content, format)
+      console.log('File created successfully: ', name);
+  } catch (error) {
+      console.error('Error creating file: ', error);
+      process.exit(1);
+  }
 }
 
-module.exports = helloNpm
+// Backend Creation
+const createFrontend = () => {
+  fs.mkdir('./frontend', { recursive: true}, (err) => {
+    if (err) throw err;
+  });
+}
+
+// const BACKEND_FOLDERS = ['controllers', 'routes', 'models', 'config',] // TO-DO OPTIMIZATION
+
+const createBackend = async () => {
+  try {
+    await fs.mkdir('./backend');
+
+    await fs.mkdir('./backend/controllers');
+    await fs.mkdir('./backend/routes');
+    await fs.mkdir('./backend/models');
+    await fs.mkdir('./backend/config');
+    await fs.mkdir('./backend/middleware');
+
+  } catch(err) {
+    console.error("Unable to create backend folder", err);
+    process.exit(1);
+  }
+}
+
+
+// const userInput = await select({
+//   message: 'Create frontend or backend?',
+//   choices: [
+//     'frontend',
+//     'backend',
+//   ],
+// });
+
+// const choice = userInput === 'frontend' ? createFrontend() : createBackend();
+
+// let file;
+// try {
+//    file = await open("textfile.txt", 'r');
+// } finally {
+//   console.log(file, 'asdf')
+//   await file?.close();
+// }
+
+createBackend();
